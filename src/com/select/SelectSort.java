45a32e068b1eed2fa396e113394c5f5c0031f267
pyr <15501163038@163.com>
2016-06-02 15:57:11 +0800

    update select

--- src/com/select/SelectSort.java
+++ src/com/select/SelectSort.java
@@ -1,5 +1,33 @@
 package com.select;
 
 public class SelectSort {
-
+	public static void main(String[] args){
+		int[] a={3,5,6,1,8,4,2,6,9};
+		selectSort(a);
+		for(int s:a){
+			System.out.println(s);
+		}
+	}
+	
+	public static void selectSort(int[] a){
+		int tmp=0;
+		int index=0;
+		int len=a.length;
+		for(int i=0;i<len;i++){
+			tmp=a[i];
+			index=i;
+			for(int j=i+1;j<len;j++){
+				if(a[j]<tmp){
+					index=j;
+					tmp=a[j];
+				}
+			}
+			if(index!=i){
+				a[index]=a[i];
+				a[i]=tmp;
+			}
+			
+		}
+	}
+	
 }
