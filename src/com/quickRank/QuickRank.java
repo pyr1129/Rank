de9bde3ba1be872fac7e657bcb19e4061d135a09
pyr <15501163038@163.com>
2016-06-02 14:53:41 +0800

    a better quickRank

--- src/com/quickRank/QuickRank.java
+++ src/com/quickRank/QuickRank.java
@@ -13,8 +13,8 @@ public class QuickRank {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a[] = { 8,5,6,9,1,11,2,10,7,4 };
-		quickRank(0, 9, a);
+		int a[] = {3,17,3,8,5,6,9,1,11,2,10,7,4 };
+		quickRank(0,11, a);
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
@@ -42,36 +42,24 @@ public class QuickRank {
 	}
 
 	public static void quickRank(int i, int j, int a[]) {
-		if (i > j) {
+		if(i>=j)
 			return;
+		int start=i;
+		int end=j;
+		int flag=a[i];
+		while(i<j){
+			while(i<j&&a[j]>=flag)
+				j--;
+			if(i<j)
+				a[i++]=a[j];
+			while(i<j&&a[i]<flag)
+				i++;
+			if(i<j)
+				a[j--]=a[i];
 		}
-		int left, right, temp, flag;
-		left = i;
-		right = j;
-
-		flag = a[i];
-
-		while (left != right) {
-
-			while (a[right] >= flag && left < right)
-				right--;
-
-			while (a[left] <= flag && left < right)
-				left++;
-			
-			if (left < right) {
-				temp = a[left];
-				a[left] = a[right];
-				a[right] = temp;
-			}
-
-		}
-		System.out.println(left+"	"+flag);
-		a[i] = a[left];
-		a[left] = flag;
-		quickRank(i, left-1, a);
-		quickRank(left+1 , j, a);
-		return;
+		a[i]=flag;
+		quickRank(start,i-1,a);
+		quickRank(i+1, end, a);
 	}
 
 }
